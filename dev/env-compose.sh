58a7d0b7280f3d383e0c94dd55460dda3491d9ea
Aaron Cameron <aaron.cameron@workiva.com>
2024-08-26 11:23:42 -0600

    Capture Docker logs

--- dev/env-compose.sh
+++ dev/env-compose.sh
@@ -2,6 +2,13 @@
 
 set -e
 docker compose -f ./dev/compose.yml up -d
+
+# Set up the logging
+mkdir -p ./dev/logs
+for service in frc-codex-processor frc-codex-server; do
+  docker compose -f ./dev/compose.yml logs -f $service > ./dev/logs/$service.log 2>&1 &
+done
+
 echo "Waiting for healthy services..."
 until [ -z "$(docker ps --format '{{.Names}}: {{.Status}}' | grep -v '(healthy)')" ]; do
   if docker ps --format '{{.Names}}: {{.Status}}' | grep '(unhealthy)'; then
