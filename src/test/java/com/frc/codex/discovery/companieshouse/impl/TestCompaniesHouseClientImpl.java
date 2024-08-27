a96fe65c424f79f414516f9088ec09bb393528f0
Aaron Cameron <aaron.cameron@workiva.com>
2024-08-27 17:29:50 -0600

    Updates for test environment

--- src/test/java/com/frc/codex/discovery/companieshouse/impl/TestCompaniesHouseClientImpl.java
+++ src/test/java/com/frc/codex/discovery/companieshouse/impl/TestCompaniesHouseClientImpl.java
@@ -1,28 +1,13 @@
 package com.frc.codex.discovery.companieshouse.impl;
 
-import static java.util.Objects.requireNonNull;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.frc.codex.discovery.companieshouse.CompaniesHouseClient;
-import com.frc.codex.discovery.companieshouse.CompaniesHouseConfig;
-
-import jdk.jshell.spi.ExecutionControl;
 
 @Component
 @Profile("test")
@@ -44,7 +29,11 @@ public class TestCompaniesHouseClientImpl implements CompaniesHouseClient {
 		return null;
 	}
 
-	public void streamFilings(Long timepoint, Function<String, Boolean> callback) {
+	public List<String> streamFilings(long maxMs) {
+		return null;
+	}
+
+	public void streamFilings(Function<String, Boolean> callback) {
 
 	}
 }
