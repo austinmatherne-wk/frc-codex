baf2df08260464a9337ec4dae80c31b7e91df083
Aaron Cameron <aaron.cameron@workiva.com>
2024-09-05 11:50:45 -0600

    Update Docker/compose for processing and viewing filings

--- dev/localstack.sh
+++ dev/localstack.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-
+# Create SQS Queues
 echo "Initializing localstack SQS: frc_codex_jobs"
 awslocal sqs create-queue --queue-name frc_codex_jobs --region "$SQS_REGION_NAME"
 echo "Initializing localstack SQS: frc_codex_results"
 awslocal sqs create-queue --queue-name frc_codex_results --region "$SQS_REGION_NAME"
+
+# Create S3 Bucket
+echo "Initializing localstack S3: $S3_BUCKET_NAME"
+awslocal s3 mb "s3://$S3_BUCKET_NAME" --region "$S3_REGION_NAME"
