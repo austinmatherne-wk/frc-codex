139d2db431f69e7cd54703a6f9da80b46da50886
Aaron Cameron <aaron.cameron@workiva.com>
2024-09-10 15:04:29 -0600

    "Load more" link

--- src/main/java/com/frc/codex/filingindex/controller/HomeController.java
+++ src/main/java/com/frc/codex/filingindex/controller/HomeController.java
@@ -15,6 +15,8 @@ import com.frc.codex.model.SearchFilingsRequest;
 
 @Controller
 public class HomeController {
+	private static final int MAX_SEARCH_RESULTS = 100;
+
 	private final DatabaseManager databaseManager;
 
 	public HomeController(
@@ -33,21 +35,36 @@ public class HomeController {
 		searchFilingsRequest.setStatus(FilingStatus.COMPLETED.toString());
 		List<Filing> filings;
 		String message;
+		boolean maximumResultsReturned = false;
+		String moreResultsLink = null;
 		if (searchFilingsRequest.isEmpty()) {
 			filings = null;
 			message = "Please provide at least one search criterion.";
 		} else {
+			searchFilingsRequest.setLimit(
+					Math.max(10, Math.min(searchFilingsRequest.getLimit(), MAX_SEARCH_RESULTS))
+			);
 			filings = databaseManager.searchFilings(searchFilingsRequest);
 			if (filings.isEmpty()) {
 				message = "No filings matched your search criteria.";
 			} else {
 				message = null;
+				if (filings.size() >= MAX_SEARCH_RESULTS) {
+					maximumResultsReturned = true;
+				} else if (filings.size() >= searchFilingsRequest.getLimit()) {
+					moreResultsLink = "/?companyName=" + searchFilingsRequest.getCompanyName() +
+							"&companyNumber=" + searchFilingsRequest.getCompanyNumber() +
+							"&limit=" + (searchFilingsRequest.getLimit() + 10) +
+							"#result-" + (filings.size() - 1);
+				}
 			}
 		}
 		ModelAndView model = new ModelAndView("index");
 		model.addObject("filings", filings);
 		model.addObject("message", message);
 		model.addObject("searchFilingsRequest", searchFilingsRequest);
+		model.addObject("maximumResultsReturned", maximumResultsReturned);
+		model.addObject("moreResultsLink", moreResultsLink);
 		return model;
 	}
 }
