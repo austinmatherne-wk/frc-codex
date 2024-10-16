7d7fe34ee6db47da746bb55083ace62280da6611
Austin M. Matherne <Austin.Matherne@Workiva.com>
2024-10-16 18:54:22 -0400

    Make sure _HTTP_CACHE directory exists for lambda build

--- dev/env-build.sh
+++ dev/env-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 set -e
+# Create HTTP_CACHE directory if it doesn't already exist.
+mkdir -p ./_HTTP_CACHE
 docker build -t frc-codex-lambda --platform linux/amd64 -f frc-codex-lambda.Dockerfile .
 docker build -t frc-codex-processor -f frc-codex-processor.Dockerfile .
 docker build -t frc-codex-server -f frc-codex-server.Dockerfile .
