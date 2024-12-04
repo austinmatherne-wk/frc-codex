e7885b229d1a18188f50a3e4d65c56b843fa4945
Austin M. Matherne <Austin.Matherne@Workiva.com>
2024-12-04 12:31:49 -0500

    Remove help form email code
    The help form has been replaced by a simple mailto link.

--- src/main/java/com/frc/codex/support/impl/SupportManagerImpl.java
+++ src/main/java/com/frc/codex/support/impl/SupportManagerImpl.java
@@ -11,23 +11,12 @@ import com.frc.codex.model.HelpRequest;
 import com.frc.codex.model.SurveyRequest;
 import com.frc.codex.support.SupportManager;
 
-import software.amazon.awssdk.services.ses.SesClient;
-import software.amazon.awssdk.services.ses.model.Body;
-import software.amazon.awssdk.services.ses.model.Content;
-import software.amazon.awssdk.services.ses.model.Destination;
-import software.amazon.awssdk.services.ses.model.Message;
-import software.amazon.awssdk.services.ses.model.SendEmailRequest;
-import software.amazon.awssdk.services.ses.model.SendEmailResponse;
-import software.amazon.awssdk.services.ses.model.SesException;
-
 @Component
 public class SupportManagerImpl implements SupportManager {
 	private static final Logger LOG = LoggerFactory.getLogger(SupportManagerImpl.class);
-	private final SesClient sesClient;
 	private final String supportEmail;
 
 	public SupportManagerImpl(FilingIndexProperties properties) {
-		this.sesClient = SesClient.builder().build();
 		this.supportEmail = properties.supportEmail();
 	}
 
